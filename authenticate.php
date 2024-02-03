a1ee249abf1f07e3aa71e961a96a3406f3dc91b7
jjamk <59911373+jjamk@users.noreply.github.com>
2024-02-03 23:04:56 +0900

    Add files via upload

--- authenticate.php
+++ authenticate.php
@@ -28,7 +28,7 @@
 		if ($result->num_rows > 0) {
 			session_start();
 			$_SESSION["username"] = $username;
-			echo "<script>alert('로그인에 성공하였습니다');</script>";
+			echo "<script>alert('" . htmlspecialchars($username, ENT_QUOTES, 'UTF-8') . "님, 환영합니다'); history.back(-1);</script>";
 			//header("location: webshell.php");
 			exit();
 		} else {
